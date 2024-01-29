069c8e74398329a85edc9cca27398fe04352e074
Umar Shabazov <malik95i@yandex.ru>
2024-01-29 22:25:05 +0400

    Initial commit

---
+++ src/test/java/com/github/umarshabazov/firstspring/FirstSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.umarshabazov.firstspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
