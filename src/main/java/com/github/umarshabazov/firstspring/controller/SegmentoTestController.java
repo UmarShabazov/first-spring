c612f5a8bf7d20032f0ad24dcc60dbe3dc70d9e4
Umar Shabazov <malik95i@yandex.ru>
2024-02-11 15:30:00 +0300

    SegmentoTest init

---
+++ src/main/java/com/github/umarshabazov/firstspring/controller/SegmentoTestController.java
@@ -0,0 +1,30 @@
+package com.github.umarshabazov.firstspring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+public class SegmentoTestController {
+
+    public List <String> url = new ArrayList<>();
+
+    @GetMapping ("/add/{anyUrl}")
+    public void addUrl (@PathVariable String anyUrl) {
+
+        url.add(anyUrl);
+    }
+
+
+    @GetMapping ("/top/{integer}")
+    public List <String> getUrl (@PathVariable int integer) {
+
+        return Collections.singletonList(url.get(0));
+        //Должен возвращать самые популярные домены, пока оставлю по индексу, надо доработать
+    }
+}
