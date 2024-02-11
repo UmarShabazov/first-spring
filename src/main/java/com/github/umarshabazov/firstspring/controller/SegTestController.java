cff2ad65a55916d9193d740e4616aaf71827f9b5
Umar Shabazov <malik95i@yandex.ru>
2024-02-11 15:34:59 +0300

    SegTest init

--- src/main/java/com/github/umarshabazov/firstspring/controller/SegTestController.java
+++ src/main/java/com/github/umarshabazov/firstspring/controller/SegTestController.java
@@ -2,7 +2,6 @@ package com.github.umarshabazov.firstspring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -10,7 +9,7 @@ import java.util.Collections;
 import java.util.List;
 
 @RestController
-public class SegmentoTestController {
+public class SegTestController {
 
     public List <String> url = new ArrayList<>();
 
