2489f7f3b558db6b0910bb979b1b50d4167ae929
Umar Shabazov <malik95i@yandex.ru>
2024-02-13 00:32:30 +0300

    BetaVersion of SegTest

--- src/main/java/com/github/umarshabazov/firstspring/controller/SegTestController.java
+++ src/main/java/com/github/umarshabazov/firstspring/controller/SegTestController.java
@@ -1,29 +1,62 @@
 package com.github.umarshabazov.firstspring.controller;
 
+import org.springframework.context.annotation.EnableMBeanExport;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @RestController
 public class SegTestController {
 
-    public List <String> url = new ArrayList<>();
+    public HashMap<String, Integer> url = new HashMap<>();
 
-    @GetMapping ("/add/{anyUrl}")
-    public void addUrl (@PathVariable String anyUrl) {
+    @GetMapping("/add/{anyName}")
+    public void addUrl(@PathVariable String anyName) {
 
-        url.add(anyUrl);
+        if (url.containsKey(anyName)) {
+            url.put(anyName, url.get(anyName) + 1);
+        } else {
+            url.put(anyName, 1);
+        }
     }
 
 
-    @GetMapping ("/top/{integer}")
-    public List <String> getUrl (@PathVariable int integer) {
+    @GetMapping("/top/{anyInteger}")
+    public String[] getUrl(@PathVariable Integer anyInteger) {
 
-        return Collections.singletonList(url.get(0));
-        //Должен возвращать самые популярные домены, пока оставлю по индексу, надо доработать
+        List<Integer> values = new ArrayList<>(url.values());
+        Integer[] arr = new Integer[values.size()];
+        values.toArray(arr);
+        String[] strings = new String[anyInteger];
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[minIndex] < arr[j]) {
+                    minIndex = j;
+                }
+            }
+            int tmp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = tmp;
+        }
+
+        if (anyInteger > 0) {
+
+            for (Map.Entry<String, Integer> stringIntegerEntry : url.entrySet()) {
+
+                for (int k = 0; k < anyInteger; k++) {
+                    if (stringIntegerEntry.getValue().equals(arr[k])) {
+                        strings[k] = stringIntegerEntry.getKey();
+                    }
+                }
+            }
+
+
+        }
+        return strings;
     }
 }
